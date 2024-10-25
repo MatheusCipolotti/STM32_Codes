fe80cdd3042391bb7d291344c70fa3137c134e94
Matheus Dantas Cipolotti <102692462+MatheusCipolotti@users.noreply.github.com>
2024-10-25 07:39:45 -0300

    Delete STM32F030K6T6/Entrada_Digital/PROG directory

--- STM32F030K6T6/Entrada_Digital/PROG/Inc/configs.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIGS_H_
-#define CONFIGS_H_
-
-void OscInit(void);
-void GPIO(void);
-
-#endif /* CONFIGS_H_ */
--- STM32F030K6T6/Entrada_Digital/PROG/Inc/delay.h
+++
@@ -1,6 +0,0 @@
-#ifndef DELAY_H_
-#define DELAY_H_
-
-void Delay_ms(int Vezes);
-
-#endif /* DELAY_H_ */
