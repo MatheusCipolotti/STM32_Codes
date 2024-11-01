ab8e0af29d35ff9ad00ce995aa83ac28faa9cb06
Matheus Dantas Cipolotti <102692462+MatheusCipolotti@users.noreply.github.com>
2024-10-31 22:27:49 -0300

    Update configs.c

--- STM32F030K6T6/PWM/Software/Src/configs.c
+++ STM32F030K6T6/PWM/Software/Src/configs.c
@@ -15,6 +15,11 @@ void GPIO(void){
 	//Ativa o clock para o GPIOA
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
+	//Configura o PA7 como saida alternativa para o TIM1_CH1N (AF2)
+	GPIOA->MODER &= ~(GPIO_MODER_MODER7);
+	GPIOA->MODER |= (GPIO_MODER_MODER7_1); 						//PA7 em modo função alternativa
+	GPIOA->AFR[0] |= (0x02 << GPIO_AFRH_AFRH7_Pos); 				//AF2 para PA7
+
 	//Configura o PA8 como saida alternativa para o TIM1_CH1 (AF2)
 	GPIOA->MODER &= ~(GPIO_MODER_MODER8);
 	GPIOA->MODER |= (GPIO_MODER_MODER8_1); 						//PA8 em modo função alternativa
@@ -33,18 +38,13 @@ void GPIO(void){
 	//Ativa o clock para o GPIOB
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
 
-	//Configura o PB13 como saida alternativa para o TIM1_CH1N (AF2)
-	GPIOB->MODER &= ~(GPIO_MODER_MODER13);
-	GPIOA->MODER |= (GPIO_MODER_MODER13_1); 					//PB13 em modo função alternativa
-	GPIOB->AFR[1] |= (0x02 << GPIO_AFRH_AFRH5_Pos); 				//AF2 para PB13
+	//Configura o PB0 como saida alternativa para o TIM1_CH2N (AF2)
+	GPIOB->MODER &= ~(GPIO_MODER_MODER0);
+	GPIOB->MODER |= (GPIO_MODER_MODER0_1); 						//PB0 em modo função alternativa
+	GPIOB->AFR[0] |= (0x02 << GPIO_AFRH_AFRH0_Pos); 				//AF2 para PB0
 
-	//Configura o PB14 como saida alternativa para o TIM1_CH2N (AF2)
-	GPIOB->MODER &= ~(GPIO_MODER_MODER14);
-	GPIOA->MODER |= (GPIO_MODER_MODER14_1); 					//PB14 em modo função alternativa
-	GPIOB->AFR[1] |= (0x02 << GPIO_AFRH_AFRH6_Pos); 				//AF2 para PB14
-
-	//Configura o PB15 como saida alternativa para o TIM1_CH2N (AF2)
-	GPIOB->MODER &= ~(GPIO_MODER_MODER15);
-	GPIOA->MODER |= (GPIO_MODER_MODER15_1); 					//PB15 em modo função alternativa
-	GPIOB->AFR[1] |= (0x02 << GPIO_AFRH_AFRH7_Pos); 				//AF2 para PB15
+	//Configura o PB1 como saida alternativa para o TIM1_CH2N (AF2)
+	GPIOB->MODER &= ~(GPIO_MODER_MODER1);
+	GPIOB->MODER |= (GPIO_MODER_MODER1_1); 						//PB1 em modo função alternativa
+	GPIOB->AFR[0] |= (0x02 << GPIO_AFRH_AFRH1_Pos); 				//AF2 para PB1	
 }
