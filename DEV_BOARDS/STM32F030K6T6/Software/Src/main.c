3c9e5677a4dbca1813632973c6410769e68a23f4
Matheus Dantas Cipolotti <102692462+MatheusCipolotti@users.noreply.github.com>
2024-11-21 21:19:39 -0300

    Update main.c

--- DEV_BOARDS/STM32F030K6T6/Software/Src/main.c
+++ DEV_BOARDS/STM32F030K6T6/Software/Src/main.c
@@ -7,12 +7,12 @@
 unsigned char uart_rd = 0x00;
 
 int main(){
-	OscInit();											//Configura o microcontrolador para funcionar em 8MHz
+	OscInit();								//Configura o microcontrolador para funcionar em 8MHz
 	GPIO();
-	EUSART1_Init(9600);									//Configura a UART com BaudRate em 9600
+	EUSART1_Init(9600);							//Configura a UART com BaudRate em 9600
 
 	while(1){
-		GPIOB->ODR |= GPIO_ODR_0;  						//Seta PB0 em 1
+		GPIOB->ODR |= GPIO_ODR_0;  					//Seta PB0 em 1
 		EUSART1_Write_Text_ln("TESTE - BOARDLED = 1");
 		Delay_ms(500);
 		GPIOB->ODR &= ~GPIO_ODR_0;  					//Seta PB0 em 0
